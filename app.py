97d450e25d3a190da71583034eddbed556ab5716
ShriyanshPandey <91411279+ShriyanshPandey@users.noreply.github.com>
2025-03-04 20:33:38 +0530

    Updated port in app.py

--- app.py
+++ app.py
@@ -42,4 +42,4 @@ def rate_joke():
     return render_template('index.html', joke=joke, rating=f'You rated last joke as {rating} stars!')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
